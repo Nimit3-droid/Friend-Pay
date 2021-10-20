0513667f2ba66f2a1724103f26261f17a957bcf3
Nimit3-droid <nimitkhanna844@gmail.com>
2021-10-21 02:25:53 +0530

    Updates

---
+++ precache-manifest.3cbe5d4ba35bbd48aeaab31be43e6627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca2e6d29cb8fd68bbba240ec4d76cfc",
+    "url": "/Friend-Pay/src/index.js/index.html"
+  },
+  {
+    "revision": "55c250f33437fb0e445b",
+    "url": "/Friend-Pay/src/index.js/static/css/main.8c48e2df.chunk.css"
+  },
+  {
+    "revision": "e7c19ad7663780dc4e5f",
+    "url": "/Friend-Pay/src/index.js/static/js/2.e8ad3a44.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Friend-Pay/src/index.js/static/js/2.e8ad3a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c250f33437fb0e445b",
+    "url": "/Friend-Pay/src/index.js/static/js/main.060344d8.chunk.js"
+  },
+  {
+    "revision": "547161e2394a1f3bd123",
+    "url": "/Friend-Pay/src/index.js/static/js/runtime-main.9f12c732.js"
+  }
+]);
